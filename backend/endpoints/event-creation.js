96fbdbca73e6f3fcced30d15b362f6a21afd2357
Ina Cebrucean <Ina.vjx@gmail.com>
2025-10-10 18:07:12 -0400

    [Event Creation] Task.#30 - Implement Link Between Organiser Dashboard and Event Creation

--- backend/endpoints/event-creation.js
+++ backend/endpoints/event-creation.js
@@ -5,10 +5,6 @@ module.exports = (client) => {
     router.get("/", async (req, res) => {
         res.send("Hello world from event creation");
     });
-    
-    router.post("/", async (req, res) => {
-        res.send("Event creation endpoint is working");
-    });
 
     return router;
 };
