0df0d6dffa530ff777996ca7856e50be99f4b7ea
TheBester14 <40973175+TheBester14@users.noreply.github.com>
2025-11-12 09:23:39 -0500

    Task.#88: Add ESLint to analyze code quality and find errors

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.js"],
+
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+
+    extends: [js.configs.recommended],
+
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "off",
+    },
+  },
+]);
